39fe69c58b763928d25b99814767327eba9d3390
Ulan Djamanbalaev <udjamanbalaev@gmail.com>
2018-12-18 18:35:08 +0600

    Add sneakers and do appropriate settings

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'dotenv-rails'
 
 gem 'shopify_api'
+gem 'sneakers', '~> 2.7'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ config/initializers/sneakers.rb
@@ -0,0 +1,13 @@
+require 'sneakers'
+
+Sneakers.configure(
+    heartbeat: 10,
+    amqp: Rails.application.credentials[Rails.env.to_sym][:amqp_url],
+    prefetch: 10,
+    workers: ENV['WORKERS_COUNT'] ? ENV['WORKERS_COUNT'].to_i : 1,
+    threads: ENV['WORKERS_THREADS'] ? ENV['WORKERS_THREADS'].to_i : 7,
+    env: ENV['RACK_ENV'],
+    durable: true
+)
+
+Sneakers.logger.level = Logger::INFO
